b2baa360a183ea4209f06fb77176e94c30eaf099
AJC <aleccampos10@yahoo.com>
2022-12-02 17:26:01 -0600

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -9,15 +9,20 @@ builder.Services.AddControllers();
 //The opt are the options we receive from the DataContext method, we will then use the lambda expression to create an
 //anonymous function
 //*The type DataContext comes from the class we created that holds the getter and setter for the user (id, name)
-builder.Services.AddDbContext<DataContext>(opt => 
+builder.Services.AddDbContext<DataContext>(opt =>
 {
     //The DefaultConnection comes from the appsettings development json file 
     //*"DefaultConnection": "Data source=datingapp.db "
     //The string is the connection String
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
+
+//configure the HTTP request pipeline
+app.UseCors((builder) => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
 
 // Configure the HTTP request pipeline.
 app.MapControllers();
